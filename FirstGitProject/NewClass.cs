07eea67ece14f624b6abaade10c776e9139fb395
BohdanLytvynov <blitvinov19@gmail.com>
2022-11-08 20:22:32 +0200

    3Commit

--- FirstGitProject/NewClass.cs
+++ FirstGitProject/NewClass.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FirstGitProject
 {
-    internal class NewClass
+    public class NewClass
     {
     }
 }
--- FirstGitProject/Program.cs
+++ FirstGitProject/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using FirstGitProject;
+
 Console.WriteLine("Hello, World!");
+
+NewClass Class = new NewClass(); // Add A new Class
